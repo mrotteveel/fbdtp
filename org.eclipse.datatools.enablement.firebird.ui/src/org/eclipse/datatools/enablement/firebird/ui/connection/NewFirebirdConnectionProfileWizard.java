85285511decf0a4011462d77b70e1b71a2e4a9ac
Mark Rotteveel <mark@lawinegevaar.nl>
2009-07-05 12:30:28 +0000

    Set title of window

--- org.eclipse.datatools.enablement.firebird.ui/src/org/eclipse/datatools/enablement/firebird/ui/connection/NewFirebirdConnectionProfileWizard.java
+++ org.eclipse.datatools.enablement.firebird.ui/src/org/eclipse/datatools/enablement/firebird/ui/connection/NewFirebirdConnectionProfileWizard.java
@@ -1,12 +1,14 @@
 package org.eclipse.datatools.enablement.firebird.ui.connection;
 
+import org.eclipse.datatools.connectivity.db.generic.GenericDBPlugin;
 import org.eclipse.datatools.connectivity.ui.wizards.ExtensibleNewConnectionProfileWizard;
 
 public class NewFirebirdConnectionProfileWizard extends
-		ExtensibleNewConnectionProfileWizard {
-	public NewFirebirdConnectionProfileWizard() {
-		super(
-				new FirebirdProfileDetailsWizardPage(
-						"org.eclipse.datatools.enablement.firebird.ui.connection.FirebirdProfileDetailsWizardPage")); //$NON-NLS-1$
-	}
+        ExtensibleNewConnectionProfileWizard {
+
+    public NewFirebirdConnectionProfileWizard() {
+        super(new FirebirdProfileDetailsWizardPage("detailsPage"));
+        setWindowTitle(GenericDBPlugin.getDefault().getResourceString(
+                "NewConnectionProfileWizard.title")); //$NON-NLS-1$
+    }
 }
