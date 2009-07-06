a3b68d92d69aeb8ac871eb0d58c67a6eea9c9b94
Mark Rotteveel <mark@lawinegevaar.nl>
2009-07-06 12:01:56 +0000

    Add correct synchronization for loading + code cleanup

--- org.eclipse.datatools.enablement.firebird/src/org/eclipse/datatools/enablement/firebird/catalog/FirebirdView.java
+++ org.eclipse.datatools.enablement.firebird/src/org/eclipse/datatools/enablement/firebird/catalog/FirebirdView.java
@@ -9,70 +9,65 @@ import org.eclipse.datatools.enablement.firebird.Activator;
 import org.eclipse.datatools.enablement.firebird.catalog.loader.FirebirdTableLoader;
 import org.eclipse.datatools.modelbase.sql.expressions.QueryExpression;
 import org.eclipse.datatools.modelbase.sql.expressions.impl.QueryExpressionDefaultImpl;
+import org.eclipse.datatools.modelbase.sql.tables.SQLTablesPackage;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EStructuralFeature;
 
 /**
  * 
  * @author Roman Rokytskyy
+ * @author Mark Rotteveel
  * 
  */
 public class FirebirdView extends JDBCView {
 
-// TODO Remove or find usage
-//	private boolean showSystemObjects;
-
-	private Object queryMutex = new Object();
+	private final Object queryMutex = new Object();
 	private boolean queryLoaded = false;
 	private String query;
 
-	private Object triggersMutex = new Object();
+	private final Object triggersMutex = new Object();
 	private boolean triggersLoaded = false;
 
 	public void refresh() {
 		synchronized (triggersMutex) {
 			triggersLoaded = false;
 		}
-
 		synchronized (queryMutex) {
 			queryLoaded = false;
 		}
-
 		super.refresh();
 	}
 
 	public EList getTriggers() {
-
 		synchronized (triggersMutex) {
 			if (!triggersLoaded)
 				loadTriggers();
 		}
-
 		return super.getTriggers();
 	}
 
 	protected void loadTriggers() {
-		EList triggerList = super.getTriggers();
-		triggerList.clear();
-		Connection connection = this.getConnection();
-
-		boolean deliver = this.eDeliver();
-		this.eSetDeliver(false);
-
-		try {
-
-			FirebirdTableLoader.loadTriggers(connection, getSchema(), this,
-					triggerList);
-
-			this.triggersLoaded = true;
-
-		} catch (Exception e) {
-			Activator.getDefault().getLog().log(
-					new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0,
-							"Could not load the triggers for table "
-									+ this.getName(), e));
-		}
-
-		this.eSetDeliver(deliver);
+	    synchronized (triggersMutex) {
+    		EList triggerList = super.getTriggers();
+    		triggerList.clear();
+    		Connection connection = this.getConnection();
+    
+    		boolean deliver = this.eDeliver();
+    		this.eSetDeliver(false);
+    
+    		try {
+      			FirebirdTableLoader.loadTriggers(connection, getSchema(), this,
+    					triggerList);
+    
+    			triggersLoaded = true;
+       		} catch (Exception e) {
+    			Activator.getDefault().getLog().log(
+    					new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0,
+    							"Could not load the triggers for view "
+    									+ this.getName(), e));
+    		}
+    		this.eSetDeliver(deliver);
+	    }
 	}
 
 	public boolean isInsertable() {
@@ -82,48 +77,55 @@ public class FirebirdView extends JDBCView {
 	public boolean isUpdatable() {
 		return super.isUpdatable();
 	}
+	
+   public boolean eIsSet(EStructuralFeature eFeature) {
+        switch (eDerivedStructuralFeatureID(eFeature)) {
+        case SQLTablesPackage.VIEW_TABLE__TRIGGERS:
+            getTriggers();
+            break;
+        case SQLTablesPackage.VIEW_TABLE__QUERY_EXPRESSION:
+            getQueryExpression();
+            break;
+        }
+        return super.eIsSet(eFeature);
+    }
 
 	public QueryExpression getQueryExpression() {
 		synchronized (queryMutex) {
 			if (!queryLoaded)
 				loadQueryExpression();
 		}
-
 		return super.getQueryExpression();
 	}
 
 	protected void loadQueryExpression() {
-		Connection connection = this.getConnection();
-
-		boolean deliver = this.eDeliver();
-		this.eSetDeliver(false);
-
-		try {
-
-			query = FirebirdTableLoader.loadViewQuery(connection, getSchema(),
-					this);
-
-			setQueryExpression(new ViewQueryExpression(query));
-
-			this.queryLoaded = true;
-
-		} catch (Exception e) {
-			Activator.getDefault().getLog().log(
-					new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0,
-							"Could not load the query for view "
-									+ this.getName(), e));
-		}
-
-		this.eSetDeliver(deliver);
+	    synchronized (queryMutex) {
+    		Connection connection = this.getConnection();
+    
+    		boolean deliver = this.eDeliver();
+    		this.eSetDeliver(false);
+    
+    		try {
+    			query = FirebirdTableLoader.loadViewQuery(connection, getSchema(),
+    					this);
+    			setQueryExpression(new ViewQueryExpression(query));
+    
+    			queryLoaded = true;
+    		} catch (Exception e) {
+    			Activator.getDefault().getLog().log(
+    					new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0,
+    							"Could not load the query for view "
+    									+ this.getName(), e));
+    		}
+    
+    		this.eSetDeliver(deliver);
+	    }
 	}
 
 	private static class ViewQueryExpression extends QueryExpressionDefaultImpl {
 
 		public ViewQueryExpression(String query) {
-			super();
-
 			setSQL(query);
 		}
-
 	}
 }
