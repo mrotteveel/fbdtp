bfc34a6c4a2ce78ad180eb5643c3fc60f2293fe5
Mark Rotteveel <mark@lawinegevaar.nl>
2009-07-31 21:23:39 +0000

    Update comments Correct trigger resolution to allow for multi-action triggers

--- org.eclipse.datatools.enablement.firebird/src/org/eclipse/datatools/enablement/firebird/catalog/FirebirdTrigger.java
+++ org.eclipse.datatools.enablement.firebird/src/org/eclipse/datatools/enablement/firebird/catalog/FirebirdTrigger.java
@@ -20,60 +20,105 @@ import org.eclipse.datatools.modelbase.sql.tables.ActionTimeType;
 import org.eclipse.datatools.modelbase.sql.tables.impl.TriggerImpl;
 
 /**
+ * Trigger implementation for the Firebird database.
  * 
  * @author Roman Rokytskyy
  *
  */
 public class FirebirdTrigger extends TriggerImpl {
+    // TODO Implement database triggers (+ loading) (separate class?)
 
 	private boolean active;
 	private int position;
+	
+	/**
+	 * Array with the supported ACTION_TIME_TYPES<br>
+	 * index 0: AFTER_LITERAL<br>
+	 * index 1: BEFORE_LITERAL
+	 */
+	private static final ActionTimeType[] ACTION_TIME_TYPE = {
+	    ActionTimeType.AFTER_LITERAL, ActionTimeType.BEFORE_LITERAL };
 
-    /* 
-	    BI -    1 -     00 00 00 1 
-	    BIU -   17 -    00 10 00 1 
-	    BIUD -  113 -   11 10 00 1  
-	    BUD -   27 -    00 11 01 1 
-	    BU -    3 -     00 00 01 1 
-	    BD -    5 -     00 00 10 1 
-	    
-	    AI -    2 -     00 00 01 0
-	    AIU -   18 -    00 10 01 0
-	    AIUD -  114 -   11 10 01 0
-	    AUD -   28 -    00 11 10 0
-	    AU -    4 -     00 00 10 0
-	    AD -    6 -     00 00 11 0
-    */
+    /*
+     * type     v       s3 s2 s1 t
+     * BI   -   1   -   00 00 00 1
+     * BU   -   3   -   00 00 01 1
+     * BD   -   5   -   00 00 10 1
+     * BIU  -   17  -   00 10 00 1
+     * BID  -   25  -   00 11 00 1
+     * BUD  -   27  -   00 11 01 1
+     * BIUD -   113 -   11 10 00 1
+     * 
+     * AI   -   2   -   00 00 01 0
+     * AU   -   4   -   00 00 10 0
+     * AD   -   6   -   00 00 11 0
+     * AIU  -   18  -   00 10 01 0
+     * AID  -   26  -   00 11 01 0
+     * AUD  -   28  -   00 11 10 0
+     * AIUD -   114 -   11 10 01 0
+     * 
+     * Actual value (v) depends on definition order (eg order BUID = 107, BDIU = 77)
+     */
 	public void setFirebirdTriggerType(int type) {
-		int beforeAfter = type & 0x01;
-		type = type + 1;
+	    // Select time of trigger (t)
+		final int beforeAfter = type & 0x01;
+		setActionTime(ACTION_TIME_TYPE[beforeAfter]);
+		
+		// Transform before triggers to be identical to after triggers for further processing
+		type += beforeAfter;
+		// Select slot1 (s1)
 		int slot1 = (type & 0x06) >>> 1; // (type & 00000110) >> 1
+		// Select slot2 (s2)
 		int slot2 = (type & 0x18) >>> 3; // (type & 00011000) >> 3
+		// Select slot3 (s3)
 		int slot3 = (type & 0x60) >>> 5; // (type & 01100000) >> 5
 
-		setActionTime(beforeAfter == 1 ? ActionTimeType.BEFORE_LITERAL
-				: ActionTimeType.AFTER_LITERAL);
-
-		if (slot1 == 1 || slot2 == 1 || slot3 == 1)
+		if (slot1 == 1 || slot2 == 1 || slot3 == 1) {
 			setInsertType(true);
-		else if (slot1 == 2 || slot2 == 2 || slot3 == 2)
+		}
+		// NO ELSE! Firebird 1.5 and higher support multi-action triggers
+		if (slot1 == 2 || slot2 == 2 || slot3 == 2) {
 			setUpdateType(true);
-		else if (slot1 == 3 || slot2 == 3 || slot3 == 3)
+		}
+		// NO ELSE! Firebird 1.5 and higher support multi-action triggers
+		if (slot1 == 3 || slot2 == 3 || slot3 == 3) {
 			setDeleteType(true);
+		}
+
+		/* TODO Decide whether to store actual slot order (is significant in Firebird)
+		 * for DDL generation (for correct INSERT OR UPDATE OR DELETE order)
+		 */
 	}
 
+	/**
+	 * 
+	 * @return the state of the trigger (active/inactive)
+	 */
 	public boolean isActive() {
 		return active;
 	}
 
+	/**
+	 * 
+	 * @param active the state of the trigger (active/inactive)
+	 */
 	public void setActive(boolean active) {
 		this.active = active;
 	}
 
+	/**
+	 * Position of the trigger relative to other triggers.
+	 * 
+	 * @return the position
+	 */
 	public int getPosition() {
 		return position;
 	}
 
+	/**
+	 * 
+	 * @param position the position
+	 */
 	public void setPosition(int position) {
 		this.position = position;
 	}
